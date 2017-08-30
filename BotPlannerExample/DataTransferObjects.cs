5100d223d749db3d79c441842da7cf42ade6bf25
rinormaloku <rinormaloku37@gmail.com>
2017-08-31 00:21:58 +0200

    Add project files.

---
+++ BotPlannerExample/DataTransferObjects.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BotPlanner.Dialogs
+{
+    public class PlannerTask
+    {
+        public TaskDto[] value { get; set; }
+    }
+
+    public class TaskDto
+    {
+        public string planId { get; set; }
+        public string bucketId { get; set; }
+        public string title { get; set; }
+        public string orderHint { get; set; }
+        public string assigneePriority { get; set; }
+        public int percentComplete { get; set; }
+        public object startDateTime { get; set; }
+        public DateTime createdDateTime { get; set; }
+        public object dueDateTime { get; set; }
+        public bool hasDescription { get; set; }
+        public string previewType { get; set; }
+        public object completedDateTime { get; set; }
+        public object completedBy { get; set; }
+        public int referenceCount { get; set; }
+        public int checklistItemCount { get; set; }
+        public int activeChecklistItemCount { get; set; }
+        public object conversationThreadId { get; set; }
+        public string id { get; set; }
+    }
+
+}
---
+++ BotPlannerExample/Dialogs/RootDialog.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+using AuthBot;
+using AuthBot.Dialogs;
+using Flurl.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotPlanner.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<string>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> item)
+        {
+            var message = await item;
+            var resourceId = ConfigurationManager.AppSettings["ActiveDirectory.ResourceId"];
+            var accessToken = await context.GetAccessToken(resourceId);
+
+            switch (message.Text)
+            {
+                case "logon":
+                    await AuthenticateUser(context, accessToken, resourceId, message);
+                    break;
+                case "show my tasks":
+                    var tasks = await GetMyTasks(accessToken);
+                    var resultMessage = GenerateMessageFromTasks(tasks, context.MakeMessage());
+                    await context.PostAsync(resultMessage);
+                    context.Wait(MessageReceivedAsync);
+                    break;
+                default:
+                    context.Wait(MessageReceivedAsync);
+                    break;
+            }
+        }
+
+        private async Task AuthenticateUser(IDialogContext context, string accessToken, string resourceId, IMessageActivity message)
+        {
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                await context.Forward(new AzureAuthDialog(resourceId), ResumeAfterAuth, message,
+                    CancellationToken.None);
+            }
+            else
+            {
+                await context.PostAsync("You are logged in.");
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+        
+        private async Task<TaskDto[]> GetMyTasks(string accessToken)
+        {
+            return (await "https://graph.microsoft.com/v1.0/me/planner/tasks"
+                    .WithOAuthBearerToken(accessToken)
+                    .GetJsonAsync<PlannerTask>())
+                .value;
+        }
+
+        private async Task ResumeAfterAuth(IDialogContext context, IAwaitable<string> result)
+        {
+            var message = await result;
+            await context.PostAsync(message);
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private static IMessageActivity GenerateMessageFromTasks(IEnumerable<TaskDto> tasks,
+            IMessageActivity resultMessage)
+        {
+            resultMessage.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            resultMessage.Attachments = new List<Attachment>();
+
+            foreach (var task in tasks)
+            {
+                resultMessage.Attachments.Add(new HeroCard
+                {
+                    Title = task.title,
+                    Buttons = new List<CardAction>
+                    {
+                        new CardAction
+                        {
+                            Title = "Checkout in Planner",
+                            Type = ActionTypes.OpenUrl,
+                            Value = "https://goo.gl/c79zwx"
+                        },
+                        new CardAction
+                        {
+                            Title = "Details",
+                            Type = ActionTypes.PostBack,
+                            Value = "Not Implemented"
+                        },
+                    }
+                }.ToAttachment());
+            }
+            return resultMessage;
+        }
+    }
+}
